4ca7344f826de1b07252b978cada6c6500070440
Deepak Singh <malwareperseus@gmail.com>
2021-01-23 15:13:45 +0530

    Added SelectButton Component

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import SelectButton from './Components/SelectButton.js';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <SelectButton />
       <StatusBar style="auto" />
     </View>
   );
---
+++ Components/SelectButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+export default function SelectButton() {
+    return(
+        <View>
+            <Button title= "Click"></Button>
+        </View>
+    );
+}
