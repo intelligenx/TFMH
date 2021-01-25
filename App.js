44690cc3dc866e126bfade46991db4c0bcfaffc4
Deepak Singh <malwareperseus@gmail.com>
2021-01-25 17:51:03 +0530

    Refactored Selectbuttons to Buttongroup Compoenet

--- App.js
+++ App.js
@@ -2,33 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, ScrollView, _ScrollView } from 'react-native';
 import Constants from 'expo-constants';
-import SelectButton from './Components/SelectButton.js';
+
+import ButtonGroup from './Components/ButtonGroup';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container1}>
+    <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
-        <View style={styles.container}>
-          <View style={{alignSelf: 'flex-start'}}> 
-            <SelectButton gradcolour='a' title='   Something  ' />
-          </View>
-          <View style={{alignSelf: 'flex-end'}}>
-            <SelectButton gradcolour='a' title='   Something  ' />
-          </View >
-          <View style={{alignSelf: 'flex-start'}}>
-            <SelectButton gradcolour='a' title='   Something  ' />
-          </View>
-          <View style={{alignSelf: 'flex-end'}}>
-            <SelectButton gradcolour='a' title='   Something  ' />
-          </View>
-          <View style={{alignSelf: 'flex-start'}}>
-            <SelectButton gradcolour='a' title='   Something  ' />
-          </View>
-          <View style={{alignSelf: 'flex-end'}}>
-            <SelectButton gradcolour='a' title='   Something  ' />
-          </View> 
-          <StatusBar style="auto" />
-        </View>
+        <ButtonGroup />
+        <View><StatusBar style="auto" /></View>
       </ScrollView>
     </SafeAreaView>
   
@@ -36,13 +18,7 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    justifyContent:  'center',
-  },
-  container1 : {
+  container : {
     flex: 1,
     marginTop: Constants.statusBarHeight,
   },
---
+++ Components/ButtonGroup.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View, } from 'react-native';
+
+import SelectButton from './SelectButton.js';
+
+export default function ButtonGroup() {
+    return(
+        <View style={styles.container}>
+          <View style={{alignSelf: 'flex-start'}}> 
+            <SelectButton gradcolour='a' title='   Something  ' />
+          </View>
+          <View style={{alignSelf: 'flex-end'}}>
+            <SelectButton gradcolour='a' title='   Something  ' />
+          </View >
+          <View style={{alignSelf: 'flex-start'}}>
+            <SelectButton gradcolour='a' title='   Something  ' />
+          </View>
+          <View style={{alignSelf: 'flex-end'}}>
+            <SelectButton gradcolour='a' title='   Something  ' />
+          </View>
+          <View style={{alignSelf: 'flex-start'}}>
+            <SelectButton gradcolour='a' title='   Something  ' />
+          </View>
+          <View style={{alignSelf: 'flex-end'}}>
+            <SelectButton gradcolour='a' title='   Something  ' />
+          </View> 
+          
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#fff',
+      justifyContent:  'center',
+    },
+});
