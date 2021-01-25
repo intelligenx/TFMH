94db2732b9fd8cf50600ef7ccb3542f75e78b0a2
Deepak Singh <malwareperseus@gmail.com>
2021-01-25 17:33:25 +0530

    Added Scrolling

--- App.js
+++ App.js
@@ -1,32 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, _ScrollView } from 'react-native';
+import Constants from 'expo-constants';
 import SelectButton from './Components/SelectButton.js';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={{alignSelf: 'flex-start'}}> 
-        <SelectButton gradcolour='a' title='   Something  ' />
-      </View>
-      <View style={{alignSelf: 'flex-end'}}>
-        <SelectButton gradcolour='a' title='   Something  ' />
-      </View >
-      <View style={{alignSelf: 'flex-start'}}>
-        <SelectButton gradcolour='a' title='   Something  ' />
-      </View>
-      <View style={{alignSelf: 'flex-end'}}>
-        <SelectButton gradcolour='a' title='   Something  ' />
-      </View>
-      <View style={{alignSelf: 'flex-start'}}>
-        <SelectButton gradcolour='a' title='   Something  ' />
-      </View>
-      <View style={{alignSelf: 'flex-end'}}>
-        <SelectButton gradcolour='a' title='   Something  ' />
-      </View> 
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container1}>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.container}>
+          <View style={{alignSelf: 'flex-start'}}> 
+            <SelectButton gradcolour='a' title='   Something  ' />
+          </View>
+          <View style={{alignSelf: 'flex-end'}}>
+            <SelectButton gradcolour='a' title='   Something  ' />
+          </View >
+          <View style={{alignSelf: 'flex-start'}}>
+            <SelectButton gradcolour='a' title='   Something  ' />
+          </View>
+          <View style={{alignSelf: 'flex-end'}}>
+            <SelectButton gradcolour='a' title='   Something  ' />
+          </View>
+          <View style={{alignSelf: 'flex-start'}}>
+            <SelectButton gradcolour='a' title='   Something  ' />
+          </View>
+          <View style={{alignSelf: 'flex-end'}}>
+            <SelectButton gradcolour='a' title='   Something  ' />
+          </View> 
+          <StatusBar style="auto" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  
   );
 }
 
@@ -37,4 +42,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent:  'center',
   },
+  container1 : {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  scrollView : {
+    backgroundColor: 'pink',
+    marginHorizontal: 0,
+  }
 });
