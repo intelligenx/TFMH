5d88a0ac2b7ecb0fbbc5021fa2924ea277aeebc5
Deepak Singh <malwareperseus@gmail.com>
2021-01-30 18:48:50 +0530

    Added About Screen Bones

--- Routes/HomeDrawer.js
+++ Routes/HomeDrawer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
+
 import HomeStack from '../Routes/HomeStack';
+import About from '../Screens/About';
 
 const Drawer = createDrawerNavigator();
 
@@ -10,6 +12,7 @@ export default function HomeDrawer() {
         <NavigationContainer>
             <Drawer.Navigator initialRouteName='Home'>
                 <Drawer.Screen name='Home' component={HomeStack} />
+                <Drawer.Screen name='About' component={About} />
             </Drawer.Navigator>
         </NavigationContainer>
     );
